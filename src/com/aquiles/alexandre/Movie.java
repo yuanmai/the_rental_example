5d85c3ae0b65ece2d61f9b538a67ca6eb6972653
Yuan Mai <4registration@gmail.com>
2011-11-21 16:47:46 +0800

    move 2 methods to Movie

--- src/com/aquiles/alexandre/Movie.java
+++ src/com/aquiles/alexandre/Movie.java
@@ -1,6 +1,7 @@
 package com.aquiles.alexandre;
 
 public class Movie {
+
     public enum PriceCode {REGULAR, NEW_RELEASE, CHILDRENS}
 
     private final String title;
@@ -19,4 +20,28 @@ public class Movie {
         return title;
     }
 
+    int getFrequentRenterPoints(Integer daysRented) {
+        return getPriceCode() == PriceCode.NEW_RELEASE
+                && daysRented > 1 ? 2 : 1;
+    }
+
+    double getCharge(Integer daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
--- src/com/aquiles/alexandre/Rental.java
+++ src/com/aquiles/alexandre/Rental.java
@@ -23,38 +23,14 @@ public class Rental {
         Integer daysRented1 = getDaysRented();
         Movie movie1 = getMovie();
 
-        return getCharge(daysRented1, movie1);
-    }
-
-    private double getCharge(Integer daysRented, Movie movie) {
-        double result = 0;
-        switch (movie.getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return movie1.getCharge(daysRented1);
     }
 
     int getFrequentRenterPoints() {
         Integer daysRented1 = getDaysRented();
         Movie movie1 = getMovie();
 
-        return getFrequentRenterPoints(daysRented1, movie1);
+        return movie1.getFrequentRenterPoints(daysRented1);
     }
 
-    private int getFrequentRenterPoints(Integer daysRented, Movie movie1) {
-        return movie1.getPriceCode() == Movie.PriceCode.NEW_RELEASE
-                && daysRented > 1 ? 2 : 1;
-    }
 }
