7cefd1a86bd5975817a7e6157bc60ace4c78fd48
Yuan Mai <4registration@gmail.com>
2011-11-21 16:45:59 +0800

    extract two movie methods

--- src/com/aquiles/alexandre/Rental.java
+++ src/com/aquiles/alexandre/Rental.java
@@ -23,20 +23,24 @@ public class Rental {
         Integer daysRented1 = getDaysRented();
         Movie movie1 = getMovie();
 
+        return getCharge(daysRented1, movie1);
+    }
+
+    private double getCharge(Integer daysRented, Movie movie) {
         double result = 0;
-        switch (movie1.getPriceCode()) {
+        switch (movie.getPriceCode()) {
             case REGULAR:
                 result += 2;
-                if (daysRented1 > 2)
-                    result += (daysRented1 - 2) * 1.5;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
                 break;
             case NEW_RELEASE:
-                result += daysRented1 * 3;
+                result += daysRented * 3;
                 break;
             case CHILDRENS:
                 result += 1.5;
-                if (daysRented1 > 3)
-                    result += (daysRented1 - 3) * 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
                 break;
         }
         return result;
@@ -46,7 +50,11 @@ public class Rental {
         Integer daysRented1 = getDaysRented();
         Movie movie1 = getMovie();
 
+        return getFrequentRenterPoints(daysRented1, movie1);
+    }
+
+    private int getFrequentRenterPoints(Integer daysRented, Movie movie1) {
         return movie1.getPriceCode() == Movie.PriceCode.NEW_RELEASE
-                && daysRented1 > 1 ? 2 : 1;
+                && daysRented > 1 ? 2 : 1;
     }
 }
